044b7b2522ebe4edc7a3c353bb8bece290ff0735
Oleksii Khatuntsev <kaelthasmafia@outlook.com>
2020-06-11 01:04:28 +0300

    Add challenge funcs

--- venv/Func_practice_03.1.py
+++ venv/Func_practice_03.1.py
@@ -97,6 +97,50 @@ def summer_69(nums):
             flag = True
     return result
 
+# SPY GAME: Write a function that takes in a list of integers and returns True if it contains 007 in order
+
+def spy_game(nums):
+    count = 0
+    while count < len(nums) - 2:
+        if nums[count] == 0 and nums[count + 1] == 0 and nums[count + 2] == 7:
+            return True
+        else:
+            count += 1
+    return False
+
+# COUNT PRIMES: Write a function that returns the number of prime numbers that exist up to and including a given number
+
+def count_primes(num):
+    result = 0
+    for count in range(2, num + 1):
+        flag = True
+        for checknum in range(2, count):
+            if count % checknum == 0:
+                flag = False
+        if flag:
+            result += 1
+        flag = True
+    return result
+
+# PRINT BIG: Write a function that takes in a single letter, and returns a 5x5 representation of that letter
+
+def print_big(letter):
+    a = '  *\n * *\n*****\n*   *\n*   *'
+    b = '****\n*   *\n****\n*   *\n****'
+    c = ' ***\n*   *\n*\n*   *\n ***'
+    d = '****\n*   *\n*   *\n*   *\n****'
+    e = '*****\n*\n*****\n*\n*****'
+    if letter == 'a':
+        return a
+    elif letter == 'b':
+        return b
+    elif letter == 'c':
+        return c
+    elif letter == 'd':
+        return d
+    elif letter == 'e':
+        return e
+
 print(get_a_number_from_number_set(2, 4))
 print(get_a_number_from_number_set(2, 5))
 
@@ -130,3 +174,15 @@ print(blackjack(9,9,11))
 print(summer_69([1, 3, 5]))
 print(summer_69([4, 5, 6, 7, 8, 9]))
 print(summer_69([2, 1, 6, 9, 11]))
+
+print(spy_game([1,2,4,0,0,7,5]))
+print(spy_game([1,0,2,4,0,5,7]))
+print(spy_game([1,7,2,0,4,5,0]))
+
+print(count_primes(100))
+
+print(print_big('a'))
+print(print_big('b'))
+print(print_big('c'))
+print(print_big('d'))
+print(print_big('e'))
